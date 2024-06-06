10678bac170c3af7408b71d0d74a2bde8aac80c2
seyoungkwon <seyoungkwon29@gmail.com>
2024-06-06 16:16:40 +0900

    BaseEntity 추가

---
+++ src/main/java/com/develop/leavingnow/global/common/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.develop.leavingnow.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate // JPA Auditing에서 사용되며, 엔티티가 생성될 때 생성 시간을 자동으로 기록한다.
+    @Column(name = "created_time", updatable = false) // 엔티티가 생성될 때만 값이 설정된다.
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate // 엔티티가 수정될 때 마지막 수정 시간을 자동으로 기록한다.
+    @Column(name = "last_modified_time")
+    private LocalDateTime lastModifiedTime;
+
+    @Column(name = "deleted_time")
+    private LocalDateTime deletedTime;
+
+    public void recordDeletedTime() {
+        this.deletedTime = LocalDateTime.now();
+    }
+}
